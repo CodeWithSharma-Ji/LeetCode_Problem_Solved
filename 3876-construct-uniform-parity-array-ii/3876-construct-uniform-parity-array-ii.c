08977401ad600aaec434829a8064e3d826d3a764
Sonu Kumar Sharma <sonuk663541@gmail.com>
2026-09-03 22:53:05 +0530

    Time: 0 ms (100%), Space: 20.4 MB (60%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.c
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.c
@@ -1,30 +1,23 @@
-#include <stdlib.h>
 #include <stdbool.h>
 
-static int cmp(const void *a, const void *b) {
-    int x = *(const int *)a, y = *(const int *)b;
-    return (x > y) - (x < y);
-}
+bool uniformArray(int* nums1, int nums1Size) {
+    int min = nums1[0];
 
-static bool checkParity(int *nums1, int n, int T) {
-    bool hasOdd = false, hasEven = false;
-    for (int i = 0; i < n; i++) {
-        int px = nums1[i] & 1;
-        if (px != T) {
-            int needed = px ^ T;           
-            
-            if (needed == 1) {
-                if (!hasOdd) return false;
-            } else {
-                if (!hasEven) return false;
-            }
+    for (int i = 1; i < nums1Size; i++) {
+        if (nums1[i] < min) {
+            min = nums1[i];
         }
-        if (px) hasOdd = true; else hasEven = true;
     }
-    return true;
-}
 
-bool uniformArray(int *nums1, int nums1Size) {
-    qsort(nums1, nums1Size, sizeof(int), cmp);
-    return checkParity(nums1, nums1Size, 0) || checkParity(nums1, nums1Size, 1);
+    if (min % 2 == 1) {
+        return true;
+    }
+
+    for (int i = 0; i < nums1Size; i++) {
+        if (nums1[i] % 2 == 1) {
+            return false;
+        }
+    }
+
+    return true;
 }
